1e2961411809dbea7b91dbf2456a4b6889802ea9
Give <papanda.5656@outlook.com>
2022-08-29 01:00:25 +0900

    add: random.rbを追加

---
+++ random.rb
@@ -0,0 +1,11 @@
+talk_members = %w[A B C D E F]
+shuffled = talk_members.shuffle
+random_count = rand(2)
+
+if random_count == 0
+  p shuffled.slice(0, 3).sort
+  p shuffled.slice(3, 6).sort
+else
+  p shuffled.slice(0, 2).sort
+  p shuffled.slice(2, 4).sort
+end
